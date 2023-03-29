5c6098734f7da760cf9c7b7b31eccfdc76357242
Roy Ben Hayun <roy.ben.hayun@iot-arch.com>
2023-03-29 18:04:17 +0300

    MAMAN 12 b - recursion works

--- dl-experiments/maman12/maman12b.py
+++ dl-experiments/maman12/maman12b.py
@@ -54,37 +54,63 @@ def a_plus_n(scalar) -> MyScalar:
 # b3
 #
 
-def get_gradient(scalar, dict={}) -> dict:
+dict={}
+
+def __traverse_computational_graph(scalar, ext) -> float:
+    print(f"__calc_chain_rule: scalar.value={scalar.value}, scalar.grad={scalar.grad}, ext={ext} ")
+    if scalar.prev is None:
+        print(f"LEAF: scalar.value={scalar.value}, scalar.grad={scalar.grad}, ext={ext}")
+        dict[scalar] = ext
+        return
+    else:
+        print(f"PRE: scalar.value {scalar.value}")
+        d = ext * scalar.grad  # external * internal
+        dict[scalar] = ext
+        __traverse_computational_graph(scalar.prev, d)
+        print(f"POST: scalar.value {scalar.value}")
+        return
+
+def get_gradient(scalar) -> dict:
     '''
     apply the Chain Rule
     '''
-
-    if scalar.prev is None:
-        print("dx\dx: ", 1)
-        return {scalar: 1}
-    else:
-        partial_dict = get_gradient(scalar.prev, {})
-        derivative = scalar.grad * partial_dict[scalar.prev]
-        print("dy\dx: ", derivative)
-        partial_dict[scalar] = derivative
-        return partial_dict
+    __traverse_computational_graph(scalar, 1)
+    print("dict ", dict)
+    return dict
+    # if scalar.prev is None:
+    #     print("dx\dx: ", 1)
+    #     dict[scalar] = 1
+    #     return 1
+    # else:
+    #     partial = get_gradient(scalar.prev)
+    #     derivative = scalar.grad * partial
+    #     print(f"dy\dx: {derivative}, grad: {scalar.grad}")
+    #     dict[scalar] = derivative
+    #     return derivative
 
 
 if __name__ == '__main__':
-    a = MyScalar(2)  # a = 2
-    b = power(a, 2)  # a^2 db\da = 2a
-    c = exp(b)  # dc\db = e^b = e^(2a)
-    d = get_gradient(c)  # calc by chain rule
-    print(d[a])  # 1
-    print(d[b])  # 54.598150033144
-    print(d[c])  # 218.3926
-
+    print("------------------------------")
     ta = torch.tensor([2.], requires_grad=True)
+    print(f"ta: {ta}")
     tb = torch.pow(ta, 2.)
+    print(f"tb: {tb}")
     tb.retain_grad()
     tc = torch.exp(tb)
+    print(f"tc: {tc}")
     tc.retain_grad()
     td = tc.backward()
     print("ta.grad: ", ta.grad)
     print("tb.grad: ", tb.grad)
     print("tc.grad: ", tc.grad)
+    print("------------------------------")
+
+    a = MyScalar(2)  # a = 2
+    b = power(a, 2)  # a^2 db\da = 2a
+    c = exp(b)  # dc\db = e^b = e^(2a)
+    d = get_gradient(c)  # calc by chain rule
+    # FIXME: in opposite directions from torch
+    print("a.grad: ", dict[a])  # 218.3926
+    print("b.grad: ", dict[b])  # 54.598150033144
+    print("c.grad: ", dict[c])  # 1
+
