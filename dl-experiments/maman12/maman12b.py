338da0a1603fc096f5f49fcdb4f4445631b271bf
Roy Ben Hayun <roy.ben.hayun@iot-arch.com>
2023-03-29 18:11:24 +0300

    clean recursion

--- dl-experiments/maman12/maman12b.py
+++ dl-experiments/maman12/maman12b.py
@@ -54,39 +54,28 @@ def a_plus_n(scalar) -> MyScalar:
 # b3
 #
 
-dict={}
 
-def __traverse_computational_graph(scalar, ext) -> float:
-    print(f"__calc_chain_rule: scalar.value={scalar.value}, scalar.grad={scalar.grad}, ext={ext} ")
+
+def __traverse_computational_graph(scalar, ext, dict) -> float:
+    print(f"__traverse_computational_graph: scalar.value={scalar.value}, scalar.grad={scalar.grad}, ext={ext} ")
+    dict[scalar] = ext
     if scalar.prev is None:
         print(f"LEAF: scalar.value={scalar.value}, scalar.grad={scalar.grad}, ext={ext}")
-        dict[scalar] = ext
         return
     else:
-        print(f"PRE: scalar.value {scalar.value}")
-        d = ext * scalar.grad  # external * internal
-        dict[scalar] = ext
-        __traverse_computational_graph(scalar.prev, d)
-        print(f"POST: scalar.value {scalar.value}")
+        print(f"RECURSE: scalar.value {scalar.value}")
+        __traverse_computational_graph(scalar.prev, ext * scalar.grad, dict)
         return
+
 
 def get_gradient(scalar) -> dict:
     '''
     apply the Chain Rule
     '''
-    __traverse_computational_graph(scalar, 1)
+    dict = {}
+    __traverse_computational_graph(scalar, 1, dict)
     print("dict ", dict)
     return dict
-    # if scalar.prev is None:
-    #     print("dx\dx: ", 1)
-    #     dict[scalar] = 1
-    #     return 1
-    # else:
-    #     partial = get_gradient(scalar.prev)
-    #     derivative = scalar.grad * partial
-    #     print(f"dy\dx: {derivative}, grad: {scalar.grad}")
-    #     dict[scalar] = derivative
-    #     return derivative
 
 
 if __name__ == '__main__':
@@ -110,7 +99,7 @@ if __name__ == '__main__':
     c = exp(b)  # dc\db = e^b = e^(2a)
     d = get_gradient(c)  # calc by chain rule
     # FIXME: in opposite directions from torch
-    print("a.grad: ", dict[a])  # 218.3926
-    print("b.grad: ", dict[b])  # 54.598150033144
-    print("c.grad: ", dict[c])  # 1
+    print("a.grad: ", d[a])  # 218.3926
+    print("b.grad: ", d[b])  # 54.598150033144
+    print("c.grad: ", d[c])  # 1
 
