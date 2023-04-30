d1c40fba6793c1c7ebf301297dc17c23c043513a
Roy Ben Hayun <roy.ben.hayun@iot-arch.com>
2023-04-30 12:14:03 +0300

    single_batch or epochs

--- dl-experiments/maman14/FashionMNISTVanilla.py
+++ dl-experiments/maman14/FashionMNISTVanilla.py
@@ -4,6 +4,7 @@ import numpy as np
 import torchvision
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 def iterate_batch(input_tensor, labels, model, optimizer, ce_loss):
@@ -28,52 +29,76 @@ def iterate_batch(input_tensor, labels, model, optimizer, ce_loss):
     return loss.detach(), acc.detach()
 
 
-def render_accuracy_plot(batches, loss, acc, title):
-    plt.figure(figsize=(12,4))
-    plt.subplot(1,2,1)
-    plt.plot(range(batches), loss)
+def render_accuracy_plot(unit, results, loss, acc, title):
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(range(results), loss)
     plt.title("CE loss")
-    plt.xlabel("Batch Number")
-    plt.subplot(1,2,2)
-    plt.plot(range(batches), acc)
+    plt.xlabel(f"{unit} Number")
+    plt.subplot(1, 2, 2)
+    plt.plot(range(results), acc)
     avg_acc = round(float(np.average(acc)), 3)
     plt.title(f"Accuracy (avg: {avg_acc})")
-    plt.xlabel("Batch Number")
+    plt.xlabel(f"{unit} Number")
     plt.suptitle(title)
     plt.show()
 
 
-def train_fashion_mnist_nn():
+def train_fashion_mnist_nn(single_batch=True):
     train_data_transformed = torchvision.datasets.FashionMNIST(root=r"C:\work_openu\DL\temp\fashion-mnist", train=True,
                                                                download=False,
                                                                transform=torchvision.transforms.PILToTensor())
 
     batch_size = 100
-    train_dataloader = DataLoader(train_data_transformed, batch_size, shuffle=False)
+    train_dataloader = DataLoader(train_data_transformed, batch_size, shuffle=True)
     print(f"samples num: {len(train_data_transformed)}")
     print(f"classes num: {len(train_data_transformed.classes)}")
     batches = len(train_dataloader)
     print(f"batche size: {batch_size}")
     print(f"batches num: {len(train_dataloader)}")
 
-    model = nn.Sequential(nn.Flatten(),
+    model = nn.Sequential(nn.Flatten(),  # flatten dimensions with size 1
                           nn.Linear(784, 10),
                           nn.LogSoftmax(dim=1))
     print(f"model: {model}")
     optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
     ce_loss = nn.NLLLoss()
-    loss = torch.zeros(batches)
-    acc = torch.zeros(batches)
-    for batch_idx, (features, labels) in enumerate(train_dataloader):
-        print(f"{batch_idx}, {features.size()}, {labels.size()}")
-        # change type to float
-        features = features.type(torch.float)
-        loss[batch_idx], acc[batch_idx] = iterate_batch(features, labels, model, optimizer, ce_loss)
+    if single_batch:
+        loss = torch.zeros(batches)
+        acc = torch.zeros(batches)
+        for batch_idx, (features, labels) in enumerate(train_dataloader):
+            print(f"{batch_idx}, {features.size()}, {labels.size()}")
+            # change type to float
+            features = features.type(torch.float)
+            loss[batch_idx], acc[batch_idx] = iterate_batch(features, labels, model, optimizer, ce_loss)
 
-    render_accuracy_plot(batches, loss, acc, f"Fashion-MNIST: batch size: {batch_size}, network: [Linear] -> [LogSoftmax]")
+        render_accuracy_plot("Batch", batches, loss, acc,
+                             f"Fashion-MNIST single-batch (batch size: {batch_size}, nn: [Flatten] -> [Linear] -> [LogSoftmax])")
+    else:
+        num_epochs = 10
+        print(f"epochs num: {num_epochs}")
+        batch_loss = torch.zeros(batches)
+        batch_acc = torch.zeros(batches)
+        train_set_loss = torch.zeros(num_epochs)
+        train_set_acc = torch.zeros(num_epochs)
+
+        for epoch in tqdm(range(num_epochs), unit="epoch"):
+            for batch_idx, (features, labels) in enumerate(train_dataloader):
+                features = features.type(torch.float)  # change type to float
+                batch_loss, batch_acc = iterate_batch(features, labels, model, optimizer, ce_loss)
+            train_set_loss[epoch] = float(np.average(batch_loss))
+            train_set_acc[epoch] = float(np.average(batch_acc))
+            print(f"epoch avg loss: {round(float(np.average(train_set_loss[epoch])), 3)}, "
+                  f"epoch avg acc: {round(float(np.average(train_set_acc[epoch])), 3)}")
+
+        print(f"total avg loss: {round(float(np.average(train_set_loss)), 3)}, "
+              f"total avg acc: {round(float(np.average(train_set_acc)), 3)}")
+
+        render_accuracy_plot("Epoch", num_epochs, train_set_loss, train_set_acc,
+                             f"Fashion-MNIST {num_epochs} epochs (batch size: {batch_size}, nn: [Flatten] -> [Linear] -> [LogSoftmax])")
 
 
 if __name__ == '__main__':
     print("Fashion-MNIST plain vanilla")
-    train_fashion_mnist_nn()
+    train_fashion_mnist_nn(single_batch=False)
 
