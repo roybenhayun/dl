1aeb8f08f2c32ef10e1124e0d69ca3e5885e3c8d
Roy Ben Hayun <roy.ben.hayun@iot-arch.com>
2023-05-02 11:19:26 +0300

    evaluation mode

--- dl-experiments/maman14/DropNorm.py
+++ dl-experiments/maman14/DropNorm.py
@@ -58,19 +58,3 @@ class DropNorm(nn.Module):
         # NOTE: when predicting: no gamma, beta
         return y
 
-
-
-if __name__ == '__main__':
-    print("ex 2")
-
-    batch_size = 10
-    features_num = 4 * 2  # ensure even number (that can split more than once)
-    N = batch_size
-    M = features_num
-
-    dn1 = DropNorm()
-    t1 = torch.ones(size=(batch_size, 1, features_num))
-    print(f"t1: {t1.shape}")
-    ret = dn1(t1)
-    print(f"ret: {ret.shape}")
-
--- dl-experiments/maman14/FashionMNISTVanilla.py
+++ dl-experiments/maman14/FashionMNISTVanilla.py
@@ -10,7 +10,8 @@ from maman14.DropNorm import DropNorm
 
 
 def iterate_batch(input_tensor, labels, model, optimizer, ce_loss):
-    optimizer.zero_grad()
+    if model.training:
+        optimizer.zero_grad()
 
     # forward pass
     y_model = model(input_tensor)
@@ -18,17 +19,19 @@ def iterate_batch(input_tensor, labels, model, optimizer, ce_loss):
     # compute loss
     loss = ce_loss(y_model, labels.long())  # must accept long
 
-    # backpropagation - backward pass
-    loss.backward()
+    if model.training:
+        # backpropagation - backward pass
+        loss.backward()
 
-    # update network weights
-    optimizer.step()
+        # update network weights
+        optimizer.step()
 
     # count predicted labels
     predicted_labels = y_model.argmax(dim=1)
 
-    acc = (predicted_labels == labels).sum() / len(labels)
-    return loss.detach(), acc.detach()
+    total_predicted = (predicted_labels == labels).sum()
+    acc = total_predicted / len(labels)
+    return loss.detach(), acc.detach(), total_predicted
 
 
 def render_accuracy_plot(unit, results, loss, acc, title):
@@ -46,7 +49,7 @@ def render_accuracy_plot(unit, results, loss, acc, title):
     plt.show()
 
 
-def train_fashion_mnist_nn(single_batch=True, use_DropNorm=False):
+def train_and_test_fashion_mnist_nn(single_batch=True, use_DropNorm=False):
     train_data_transformed = torchvision.datasets.FashionMNIST(root=r"C:\work_openu\DL\temp\fashion-mnist", train=True,
                                                                download=False,
                                                                transform=torchvision.transforms.PILToTensor())
@@ -76,16 +79,16 @@ def train_fashion_mnist_nn(single_batch=True, use_DropNorm=False):
     print(f"model: {model}")
     module_names = '>'.join([type(module).__name__ for name, module in model.named_modules()])
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
     ce_loss = nn.NLLLoss()
     if single_batch:
         loss = torch.zeros(batches)
         acc = torch.zeros(batches)
         for batch_idx, (features, labels) in enumerate(train_dataloader):
             print(f"{batch_idx}, {features.size()}, {labels.size()}")
-            # change type to float
+            # change type to float (needed in forward pass)
             features = features.type(torch.float)
-            loss[batch_idx], acc[batch_idx] = iterate_batch(features, labels, model, optimizer, ce_loss)
+            loss[batch_idx], acc[batch_idx], _ = iterate_batch(features, labels, model, optimizer, ce_loss)
 
         render_accuracy_plot("Batch", batches, loss, acc,
                              f"Fashion-MNIST single-batch (batch size: {batch_size}, nn: {module_names})")
@@ -100,7 +103,7 @@ def train_fashion_mnist_nn(single_batch=True, use_DropNorm=False):
         for epoch in tqdm(range(num_epochs), unit="epoch"):
             for batch_idx, (features, labels) in enumerate(train_dataloader):
                 features = features.type(torch.float)  # change type to float
-                batch_loss, batch_acc = iterate_batch(features, labels, model, optimizer, ce_loss)
+                batch_loss, batch_acc, _ = iterate_batch(features, labels, model, optimizer, ce_loss)
             train_set_loss[epoch] = float(np.average(batch_loss))
             train_set_acc[epoch] = float(np.average(batch_acc))
             print(f"epoch avg loss: {round(float(np.average(train_set_loss[epoch])), 3)}, "
@@ -112,8 +115,39 @@ def train_fashion_mnist_nn(single_batch=True, use_DropNorm=False):
         render_accuracy_plot("Epoch", num_epochs, train_set_loss, train_set_acc,
                              f"Fashion-MNIST {num_epochs} epochs (batch size: {batch_size}, nn: {module_names})")
 
+    #
+    # eval with test data
+    #
+
+    test_data_transformed = torchvision.datasets.FashionMNIST(root=r"C:\work_openu\DL\temp\fashion-mnist", train=False,
+                                                              download=False,
+                                                              transform=torchvision.transforms.PILToTensor())
+    test_dataloader = DataLoader(test_data_transformed, batch_size, shuffle=True)
+    samples_num = len(test_data_transformed)
+    print(f"samples num: {samples_num}")
+    print(f"batche size: {batch_size}")
+    test_batches = len(test_dataloader)
+    print(f"batches num: {test_batches}")
+
+    model.eval()  # switch to evaluation mode
+    test_set_loss = torch.zeros(len(test_dataloader))
+    test_set_acc = torch.zeros(len(test_dataloader))
+    total_acc = 0
+    with torch.no_grad():
+        for batch_idx, (features, labels) in tqdm(enumerate(test_dataloader), unit="batch"):
+            # change type to float (needed in forward pass)
+            features = features.type(torch.float)
+            test_set_loss[batch_idx], test_set_acc[batch_idx], predicted = iterate_batch(features, labels, model, optimizer, ce_loss)
+            total_acc += predicted
+
+    print(f"avg loss: {round(float(np.average(test_set_loss)), 3)}")
+    print(f"avg acc: {round(float(np.average(test_set_acc)), 3)}")
+    print(f"total accuracy: {total_acc} / {samples_num} = {round(total_acc.item() / samples_num, 3)}")
+    render_accuracy_plot("Batch", test_batches, test_set_loss, test_set_acc,
+                         f"Fashion-MNIST test set (ACC: {round(total_acc.item() / samples_num, 3)}, nn: {module_names})")
+
 
 if __name__ == '__main__':
     print("Fashion-MNIST plain vanilla")
-    train_fashion_mnist_nn(single_batch=True, use_DropNorm=True)
+    train_and_test_fashion_mnist_nn(single_batch=True, use_DropNorm=False)
 
