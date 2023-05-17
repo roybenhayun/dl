4edcf9ce88046b4d18ef8d3fa850ce7020fec1b2
Roy Ben Hayun <roy.ben.hayun@iot-arch.com>
2023-05-17 22:45:48 +0300

    fixes

--- dl-experiments/maman14/DropNorm.py
+++ dl-experiments/maman14/DropNorm.py
@@ -17,46 +17,46 @@ class DropNorm(nn.Module):
         self.running_sigma2 = 0
 
     def forward(self, x):
+        # כלומר לפני שאתה מכניס לשכבה (האיפוס, הנרמול וכו) אתה מכפיל את כל הMים בכל שורה ויוצר קלט דו מימדי?
+        # כן זה מה שעשיתי. מצאתי את מספר הפיצ'רים והמרתי כל sample לווקטור
+
+        features_wo_batches = x[0]
         if self.training:  # see p112
             # create binary mask with half random elements as zeros
-            rand_indices = torch.randn(x.shape)
-            mask_flat = torch.reshape(torch.ones(size=rand_indices.shape), (-1,))  # flatten
+            # rand_indices = torch.ones(features_wo_batches)
+            mask_flat = torch.reshape(torch.ones(size=features_wo_batches.shape), (-1,))  # flatten
             # generate a random sample from indices [0 .. mask_flat.numel()]
-            zero_indices = np.random.choice(mask_flat.numel(), int(rand_indices.numel() * self.p), replace=False)
+            zero_indices = np.random.choice(mask_flat.numel(), int(features_wo_batches.numel() * self.p), replace=False)
             mask_flat[zero_indices] = 0  # create the 1\0 mask
-            mask = torch.reshape(mask_flat, x.shape)  # reshape to original shape
+            mask = torch.reshape(mask_flat, features_wo_batches.shape)  # reshape to original shape
 
-            # apply the mask on the batch input
+            # apply the mask on the batch input (use broadcasting)
             x = x * mask
         else:
             # see p113: weaken the signal
-            x = (1 - self.p) * x
+            #x = (1 - self.p) * x
+            pass
 
         # calculate mean (mu) and stddev (sigma square) of all non-zero elements
         # see p87
         epsilon = 10 ** -5
         if self.training:
-            # compute the mean and variance along the first dimension using the dim parameter
-            # use keepdim parameter to ensure tensors have a size of [batch#, 1]
+            x2 = x[:, mask.bool()] # <- will be without the zeroed features , or mask_flat...
+            mu = x2.mean(dim=1, keepdim=True)
+            sigma2 = x2.var(dim=1, keepdim=True)
+            # see https://github.com/Idan-Alter/OU-22961-Deep-Learning/blob/main/22961_3_2_1_normalization.ipynb
+            with torch.no_grad():
+                self.running_mu = 0.9 * self.running_mu + 0.1*mu.mean()
+                self.running_sigma2 = 0.9 * self.running_sigma2 + 0.1 * sigma2.mean()
+            x = x / (1 - self.p)
 
-            # compute the mean of non-zero elements
-            # mu = torch.sum(x, dim=1, keepdim=True) / torch.sum(x != 0, dim=1, keepdim=True)
-            # compute the variance of non-zero elements
-            # sigma2 = torch.sum((x - mu) ** 2, dim=1, keepdim=True) / torch.sum(x != 0, dim=1, keepdim=True)
-
-            mu = x.mean(dim=1, keepdim=True)
-            sigma2 = x.var(dim=1, keepdim=True)
-            self.running_mu = 0.9 * self.running_mu + 0.1*mu
-            self.running_sigma2 = 0.9 * self.running_sigma2 + 0.1 * sigma2
         else:
             # see p88: alternative mu,sigma2 for eval mode
             mu = self.running_mu
             sigma2 = self.running_sigma2
-        #xhat = (x - mu) / torch.sqrt(sigma2 + epsilon)
-        xhat = torch.where(x!=0, (x - mu) / torch.sqrt(sigma2 + epsilon), x)
+        xhat = (x - mu) / torch.sqrt(sigma2 + epsilon)
 
         # calc final Y
-        #y = self.gamma * xhat + self.beta
-        y = torch.where(x!=0, self.gamma * xhat + self.beta, xhat)
+        y = self.gamma * xhat + self.beta
         return y
 
