25100e71da7bfa1de4cc453739acbad0998d6d90
Roy Ben Hayun <roy.ben.hayun@iot-arch.com>
2023-05-23 17:34:54 +0300

    ex3

--- dl-experiments/maman15/ex3.py
+++ dl-experiments/maman15/ex3.py
@@ -1,3 +1,4 @@
+import torch
 from torch.nn import Linear
 import torchvision
 from torch.utils.data import DataLoader
@@ -5,6 +6,7 @@ import matplotlib.pyplot as plt
 import torchvision.models as models
 import torchvision.transforms as T
 
+# 3.A
 train_data_transformed = torchvision.datasets.CIFAR10(root=r"C:\work_openu\DL\temp\cifar-10", train=False,
                                                            download=True,
                                                            transform=torchvision.transforms.PILToTensor())
@@ -20,6 +22,7 @@ batches = len(train_dataloader)
 print(f"batche size: {batch_size}")
 print(f"batches num: {len(train_dataloader)}")
 
+# 3.C
 iterator = iter(train_dataloader)
 imgs, labels = next(iterator)
 class_names = train_data_transformed.classes
@@ -35,6 +38,8 @@ for i in range(10):
 
 plt.show()
 
+# 3.D
+
 #resnet18 = models.resnet18(pretrained=True)  # deprecated. see https://pytorch.org/vision/stable/models.html
 # Before using the pre-trained models, one must preprocess the image
 # (resize with right resolution/interpolation, apply inference transforms, rescale the values etc).
@@ -42,35 +47,56 @@ plt.show()
 # its weights documentation
 
 resnet18_weights = models.ResNet18_Weights.IMAGENET1K_V1
-print(resnet18_weights)
-resnet18_preprocess = resnet18_weights.transforms()
-print(resnet18_preprocess)
+print(f"ResNet weights default: {models.ResNet18_Weights.DEFAULT}")
+print(f"using: {resnet18_weights}")
+resnet18_preprocess_transforms = resnet18_weights.transforms()
+print(f"ResNet transforms: {resnet18_preprocess_transforms}")
 
 resnet18 = models.resnet18(weights=resnet18_weights)
 print("----------------------")
+print("ResNet model:\n")
 print(resnet18)
+# for name, module in resnet18.named_children():
+#     print(f"{name}, {module}")
 print("=============================")
+print("model parameters:\n")
 for param in resnet18.parameters():
-    print("disable grad: ", type(param), param.size())
-    param.requires_grad = False
+    print(type(param), param.size())
 print("----------------------")
-for name, module in resnet18.named_children():
-    print(f"{name}, {module}")
-print("----------------------")
+
 last_layers = list(resnet18.children())[-2:]
+print("model last 2 layers:\n")
 print(*last_layers, sep="\n")
 print("----------------------")
 
+# 3.E
 # see p-148
 # resnet18 Classifer: (fc): Linear(in_features=512, out_features=1000, bias=True)
 cifar10_classifier = Linear(in_features=512, out_features=len(cifar10_classes), bias=True)
-print(cifar10_classifier)
+print(f"new classifier:\n {cifar10_classifier}")
 
+#
+# 3.F
+#
+
+# 3.F.1 - transforms
 cifar10_to_resnet18_transforms = T.Compose([
+    T.ToTensor(),  # convert from [0, 255] to float [0, 1]
     T.Resize(224),
-    # TODO: range [0, 1]?
-    T.Normalize(resnet18_preprocess.mean, resnet18_preprocess.std)])
+    T.Normalize(resnet18_preprocess_transforms.mean, resnet18_preprocess_transforms.std)])
 
-print(cifar10_to_resnet18_transforms)
+# 3.F.2 - optimize only Classifier
+optimizer = torch.optim.SGD(cifar10_classifier.parameters(), lr=0.1)
 
+# 3.F.3 - disable autograd
+for param in resnet18.parameters():
+    param.requires_grad = False
 
+#
+# 3.F + 3.G : train cifar10_classifier on CIFAR-10, test on test set until reaching 70%
+#
+
+resnet18.fc = cifar10_classifier
+print(f"modified ResNet FC classifier: {resnet18.fc}")
+for param in resnet18.parameters():
+    print(f"ResNet layer requires_grad: {param.requires_grad}")
